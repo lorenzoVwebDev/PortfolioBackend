c20da0b7905b282067b9a226d034fa674ec76573
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-08 21:43:36 +0100

    updated cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,5 @@
 const whitelist = [
-    '192.168.1.101',
-    'http://127.0.0.1:3000',
-    'http://localhost:3000', 'https://lorenzovwebdev.github.io', 'https://lorenzo-viganego.com', 'https://www.lorenzo-viganego.com'
+'https://lorenzo-viganego.com', 'https://www.lorenzo-viganego.com'
 ];
 
 const corsOptions = {
@@ -13,6 +11,8 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
+    methods: ['GET', 'POST', 'OPTIONS'], // Allowing these methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Ensure you're allowing the required headers
     optionsSuccessStatus: 200
 }
 //hello
