c62edeb9bdaad34c722f711d060eb4019728218f
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-09 07:56:36 +0100

    updated cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,12 +1,11 @@
 const whitelist = [
-    'http://192.168.1.101',
-    'http://127.0.0.1:3000',
-    'http://localhost:3000', 'https://lorenzovwebdev.github.io', 'https://lorenzo-viganego.com', 'https://www.lorenzo-viganego.com'
 ];
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (origin === undefined || whitelist.indexOf(origin) !== -1) {
+        console.log(origin);
+        if (whitelist.indexOf(origin) !== -1) {
+
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
