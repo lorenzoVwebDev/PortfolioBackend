89b9e480d06e9e9e20f158c8cbd7804d913d0a46
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-10 17:50:47 +0100

    updated server

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(logger);  */
     console.log(req.origin)
 }) */
 app.use((req,res,next)=> {
-    console.log(req.ip)
+    console.log(req.remoteAddress)
     next();
 })
 app.use(cors(corsOptions));
